ea9e6c65891dd4d8bcd14fdc91ef8ae075de7d34
Bhuvankambley2003 <68515641+Bhuvankambley2003@users.noreply.github.com>
2025-03-14 14:49:57 +0530

    first commit

---
+++ invproject/urls.py
@@ -0,0 +1,53 @@
+"""
+URL configuration for invproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+"""
+URL configuration for invproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from main.views import add_inventory_item, inventory_list, stock_movement, stock_list, sensor_build, sensor_detail,generate_pdf, deployed_sensors, update_deployment_status
+from main.views_auth import login_view, logout_view, register_view, user_management
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('inventory/add/', add_inventory_item, name='add_inventory_item'),
+    path('inventory-list/', inventory_list, name='inventory_list'),
+    path('stock-movement/', stock_movement, name='stock_movement'),
+    path('stock-list/', stock_list, name='stock_list'),
+    path('sensor-build/', sensor_build, name='sensor_build'),
+    path('sensor/<int:sensor_id>/', sensor_detail, name='sensor_detail'),
+    path('', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('user-management/', user_management, name='user_management'),
+    path('generate-pdf/', generate_pdf, name='generate_pdf'),
+    path('deployed-sensors/', deployed_sensors, name='deployed_sensors'),
+    path('update-deployment/<int:pk>/', update_deployment_status, name='update_deployment_status'),
+]
---
+++ main/views_auth.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('inventory_list')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def user_management(request):
+    if not request.user.is_superuser:
+        return redirect('inventory_list')
+    users = User.objects.all()
+    return render(request, 'user_management.html', {'users': users})
